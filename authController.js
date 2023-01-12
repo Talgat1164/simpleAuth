0813815f85491dc8510a2e45f29a11ffb4546574
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-13 00:35:07 +0600

    Сохранили роли в базы данных

--- authController.js
+++ authController.js
@@ -1,6 +1,9 @@
 // Controller - нужен для декомпозиции
 // именно здесь мы описываем функции логина, регистрации и тд
 // Класс можно не создавать, но он нужен чтобы скомпоновать функции в одну сущность
+const User = require("./models/User");
+const Role = require("./models/Role");
+
 class authController {
   async registration(req, res) {
     try {
@@ -14,6 +17,12 @@ class authController {
 
   async getUsers(req, res) {
     try {
+      const userRole = new Role();
+      const adminRole = new Role({ value: "ADMIN" });
+      await userRole.save();
+      await adminRole.save();
+      // Вписали для сохранения колекции ролей в бд
+      // Костыльно но робит
       res.json("server work");
     } catch (e) {}
   }
