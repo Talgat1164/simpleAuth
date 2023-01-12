420bf0ea3a39610de415f6188a6d87151ce4285e
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-13 01:46:48 +0600

    Добавили валидации в  регистрацию

--- authRouter.js
+++ authRouter.js
@@ -4,7 +4,17 @@ const router = new Router();
 const controller = require("./authController");
 const { check } = require("express-validator");
 
-router.post("/registration", controller.registration);
+router.post(
+  "/registration",
+  [
+    check("username", "Имя пользователя не может быть пустым").notEmpty(),
+    check(
+      "password",
+      "Пароль должен быть больше 4 и меньше 10 символов"
+    ).isLength({ min: 4, max: 10 }),
+  ],
+  controller.registration
+);
 router.post("/login", controller.login);
 router.get("/users", controller.getUsers);
 
