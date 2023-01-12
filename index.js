30df490ec7f527bee0800d84b5e3b082faec2b8d
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-12 23:44:52 +0600

    Подключили БД к проекту

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 const start = async () => {
   try {
+    await mongoose.connect(
+      `mongodb+srv://Talgat:Talgat2000@cluster0.lt7qh7o.mongodb.net/?retryWrites=true&w=majority`
+    );
     app.listen(PORT, () => console.log(`server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
   }
 };
-
 start();
