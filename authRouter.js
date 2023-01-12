f7de82db29b52c1ee168965326670d2acaf04f93
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-13 02:17:26 +0600

    Сделали доступ к getUsers только админам

--- authRouter.js
+++ authRouter.js
@@ -4,6 +4,7 @@ const router = new Router();
 const controller = require("./authController");
 const { check } = require("express-validator");
 const authMiddleware = require("./middleware/authMiddleware");
+const roleMiddleware = require("./middleware/roleMiddleware");
 
 router.post(
   "/registration",
@@ -17,6 +18,6 @@ router.post(
   controller.registration
 );
 router.post("/login", controller.login);
-router.get("/users", authMiddleware, controller.getUsers);
+router.get("/users", roleMiddleware(["ADMIN"]), controller.getUsers);
 
 module.exports = router;
