0221e3d31e1ce08c0f2dfd2c51b9fc9a9d9a0787
Talgat Tokayev <59330081+Talgat1164@users.noreply.github.com>
2023-01-13 02:11:16 +0600

    Сделали доступ к getUsers только авторизованым пользоваетлям

--- authController.js
+++ authController.js
@@ -33,7 +33,7 @@ class authController {
           .json({ message: "Пользователь с таким именем уже существует" });
       }
       const hashPassword = bcrypt.hashSync(password, 7);
-      const userRole = await Role.findOne({ value: "ADMIN" });
+      const userRole = await Role.findOne({ value: "USER" });
       const user = new User({
         username,
         password: hashPassword,
--- authRouter.js
+++ authRouter.js
@@ -3,6 +3,7 @@ const router = new Router();
 // router - может прослушивать запросы типа delete, get, post ...
 const controller = require("./authController");
 const { check } = require("express-validator");
+const authMiddleware = require("./middleware/authMiddleware");
 
 router.post(
   "/registration",
@@ -16,6 +17,6 @@ router.post(
   controller.registration
 );
 router.post("/login", controller.login);
-router.get("/users", controller.getUsers);
+router.get("/users", authMiddleware, controller.getUsers);
 
 module.exports = router;
